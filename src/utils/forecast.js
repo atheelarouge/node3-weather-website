a7ce274f3d9d94c70157c6de757323514e9a9216
atheelarouge <atheelarouge@gmail.com>
2021-12-06 01:51:33 +0300

    add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
@@ -5,12 +6,12 @@ const forecast = (latitude, longitude, callback) => {
 
     request({ url, json: true },(error, {body}) => {
         if (error) {
-            callback('unable to connect to weather services', '')
+            callback('Unable to connect to weather services', '')
         } else if (body.error) {
-            callback('unable to find location', '' )
+            callback('Unable to find location', '' )
         }
         else{
-            callback('', `${body.current.weather_descriptions[0]}. it is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out`)
+            callback('', `${body.current.weather_descriptions[0]}. it is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}.`)
         }
     })
 }
